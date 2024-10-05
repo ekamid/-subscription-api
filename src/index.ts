5b09c74cd3d76c393c78af1708c5448e9106f7b1
ekamid <ebrahimkhalilamidbd@gmail.com>
2024-10-05 14:00:18 +0600

    config: pm2

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import Fastify from 'fastify';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { initDB } from './config/db';
 import { initRedis } from './config/redis';
+import dotenv from 'dotenv';
 
 // Import subscription routes
 import { subscriptionRoutes } from './routes/subscription';
 import { indexRoutes } from './routes';
+
+dotenv.config();
 
 const server = Fastify({
     logger: false
@@ -21,7 +24,9 @@ const start = async () => {
         server.register(subscriptionRoutes, { prefix: '/api/v1' });
         // await subscriptionRoutes(server);
 
-        await server.listen({ port: 3000 });
+        const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8084;
+        await server.listen({ port: PORT });
+
     } catch (err) {
         server.log.error(err);
         process.exit(1);
